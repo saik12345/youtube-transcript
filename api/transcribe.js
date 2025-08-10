a4ee643a285ccda3057c223b58c45d40b713b23e
saik12345 <saikatmukherjee.adm@gmail.com>
2025-08-10 13:32:47 +0530

    update

--- api/transcribe.js
+++ api/transcribe.js
@@ -14,7 +14,7 @@ let updatedFile = "";
 //
 async function main(rawdata) {
   const response = await ai.models.generateContent({
-    model: "gemini-2.0-flash",
+    model: "gemini-2.5-flash",
     contents: [
       {
         role: "user",

43901c933a9e220f431922b0b65233d28820c7a9
saik12345 <saikatmukherjee.adm@gmail.com>
2025-08-10 16:13:24 +0530

    updated code

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
+const { jsPDF } = window.jspdf;
+const doc = new jsPDF();
 const submit = document.getElementById("submit-link");
 const ulink = document.getElementById("ulink");
+const preview = document.getElementById("preview");
+const initial = document.getElementById("initial");
+const download = document.getElementById("download");
 let data = "";
 submit.addEventListener("click", async () => {
   if (ulink.value == "" || ulink.value == null) {
     alert("No link provided");
     return;
   }
+
+  setInterval(() => {
+    initial.innerHTML = `link sumbitted`;
+  }, 2000);
+  setInterval(() => {
+    initial.innerHTML = `getting transcription....`;
+  }, 3000);
+
   const response = await fetch(
     "https://youtube-transcript-tct1.vercel.app/api/transcribe",
     {
@@ -18,11 +31,25 @@ submit.addEventListener("click", async () => {
   );
 
   data = await response.json();
+  data = data
+    .replace(/\\n/g, " ") // removes \n
+    .replace(/\\+/g, "") // removes backslashes
+    .replace(/\s{2,}/g, " ") // remove extra spaces
+    .trim();
   console.log(data);
+
+  if (data) {
+    initial.innerHTML = "Download";
+    preview.innerHTML = `${data}`;
+  }
+
   if (data.error) {
     alert(data.error);
     return;
   }
-  // const transcript = data?.transcript?.replace("/\n/g", " ");
-  // console.log(transcript);
+});
+download.addEventListener("click", () => {
+  let lines = doc.splitTextToSize(cleanText, 180);
+  doc.text(lines, 10, 10);
+  doc.save("transcript.pdf");
 });
